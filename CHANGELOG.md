da321fb9165c59de802d850eaf61ea81fec131ca
Sliim <sliim@mailoo.org>
2014-04-28 22:32:02 +0200

    Bump version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@ This file is used to list changes made in each version of webgoat cookbook.
 ## 0.1.0:
 
 * Initial release of webgoat
+
+## 0.1.1:
+
+* Integration testing with serverspec and test-kitchen
--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ maintainer_email 'sliim@mailoo.org'
 license          'GPLv3'
 description      'Installs/Configures WebGoat application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
 depends "java"
