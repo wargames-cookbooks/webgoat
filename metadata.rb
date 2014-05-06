9c5f0586a24e9730b78639683d7130fcf72153e1
Sliim <sliim@mailoo.org>
2014-05-06 20:59:53 +0200

    Bump version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ maintainer_email 'sliim@mailoo.org'
 license          'Apache 2.0'
 description      'Installs/Configures WebGoat application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "apt"
 depends "java"
